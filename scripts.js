c4ebc9bd627cb02c71fecb9392dec9eff3dec71a
Alejae1998 <alejagr1998@gmail.com>
2023-05-31 14:43:17 -0700

    finishing practice 54

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 window.onload = function () {
   const lightMode = document.getElementById('lightBtn');
   const darkMode = document.getElementById('darkBtn');
+  const defaultFont = document.getElementById('defaultFont');
+  const largeFont = document.getElementById('largeFont');
   const body = document.body;
 
   lightMode.addEventListener('click', function () {
@@ -11,5 +13,13 @@ window.onload = function () {
   darkMode.addEventListener('click', function () {
     body.style.backgroundColor = 'black';
     body.style.color = 'white';
+  });
+
+  defaultFont.addEventListener('click', function () {
+    body.style.fontSize = '20px';
+  });
+
+  largeFont.addEventListener('click', function () {
+    body.style.fontSize = '35px';
   });
 };
